68ec463b586d38e8ed5b721f7051503f5ff8a577
anaramirez26 <160180924+anaramirez26@users.noreply.github.com>
2024-07-02 15:53:32 -0600

    anadiendo menu a rama ana

--- pruebagit.cpp
+++ pruebagit.cpp
@@ -10,3 +10,47 @@ using namespace std;
 bool yaColocado(int, int mat[3][3]);
 bool tableroCompleto(int mat[3][3]);
 void marcarNumero(int, int mat[3][3]);
+
+
+int main(void)
+{
+    int opc;
+    
+
+do {
+
+        switch (opc) {
+
+        case 1: 
+
+        break;
+
+        case 2: 
+
+        
+            
+        break; 
+
+        case 3: 
+        
+                
+        break;
+
+        case 4:
+                
+        break;
+
+        case 5: 
+                        
+        break;
+
+        default:
+
+        break;
+        }
+                
+    } while (opc != 5);
+ return 0;
+
+}
+
