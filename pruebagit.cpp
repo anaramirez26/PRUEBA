64d6bc08f945d9ac2f2a2e6fb2a9cb105727edb6
anaramirez26 <160180924+anaramirez26@users.noreply.github.com>
2024-07-02 15:51:53 -0600

    Anadiendo cuerpo de menu

--- pruebagit.cpp
+++ pruebagit.cpp
@@ -4,3 +4,52 @@
 #include "cstdlib"
 #include "unistd.h"
 using namespace std;
+
+//NUMERO YA MENCIONADO POR EL BOT YA NO VOLVERLO A DECIR
+// Prototipo de función
+bool yaColocado(int, int mat[3][3]);
+bool tableroCompleto(int mat[3][3]);
+void marcarNumero(int, int mat[3][3]);
+
+int main(void)
+{
+    int opc;
+    
+
+do {
+
+        switch (opc) {
+
+        case 1: 
+
+        break;
+
+        case 2: 
+
+        
+            
+        break; 
+
+        case 3: 
+        
+                
+        break;
+
+        case 4:
+                
+        break;
+
+        case 5: 
+                        
+        break;
+
+        default:
+
+        break;
+        }
+                
+    } while (opc != 5);
+ return 0;
+
+}
+
