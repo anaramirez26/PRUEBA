285f843fcf2fb11bc0617679c24fb7c45597588e
anaramirez26 <160180924+anaramirez26@users.noreply.github.com>
2024-07-02 15:42:08 -0600

    Anadiendo prototipo de funciones

--- pruebagit.cpp
+++ pruebagit.cpp
@@ -4,3 +4,9 @@
 #include "cstdlib"
 #include "unistd.h"
 using namespace std;
+
+//NUMERO YA MENCIONADO POR EL BOT YA NO VOLVERLO A DECIR
+// Prototipo de función
+bool yaColocado(int, int mat[3][3]);
+bool tableroCompleto(int mat[3][3]);
+void marcarNumero(int, int mat[3][3]);
